272773b29a676c8fa94886f337130763da92e83f
Rudie Dirkx <github@hotblocks.nl>
2017-12-27 17:28:20 +0100

    +composer

--- inc.bootstrap.php
+++ inc.bootstrap.php
@@ -3,13 +3,9 @@
 define('REQUEST_MICROTIME', microtime(1));
 
 require 'env.php';
+require __DIR__ . '/vendor/autoload.php';
 
-require 'inc.functions.php';
-
-require '../inc/db/db_sqlite.php'; // https://github.com/rudiedirkx/db_generic
-//$db = db_mysql::open(array('user' => 'usagerplus', 'pass' => 'usager', 'database' => 'tests'));
-$db = db_sqlite::open(array('database' => 'db/series.sqlite3'));
-
+$db = db_sqlite::open(array('database' => __DIR__ . '/db/series.sqlite3'));
 if ( !$db ) {
 	exit("<p>Que pasa, amigo!? I can't read from or write to the database! Do you have a writable ./db/ folder? <strong>No bueno!</strong></p>");
 }
