4f6f064a5badbcda2e85ad257497b86547caa9bf
Rudie Dirkx <github@hotblocks.nl>
2023-04-07 05:46:28 +0200

    env.orig.php

---
+++ env.orig.php
@@ -0,0 +1,11 @@
+<?php
+
+const TVDB_API_KEY = '94F0BD0D5948FE69';
+
+// Possible: Combined_season, SeasonNumber, DVD_season
+const TVDB_DATA_FIELD_SEASON = 'SeasonNumber';
+
+// Possible: Combined_episodenumber, EpisodeNumber, DVD_episodenumber
+const TVDB_DATA_FIELD_EPISODE = 'EpisodeNumber';
+
+const USER_ID = 1;
